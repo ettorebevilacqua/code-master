2b38a4ea7fa0da20cb1ea4b31494bffe1b793fc1
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2023-11-12 10:14:07 +0100

    move package

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,40 +1,16 @@
+import { router, publicProcedure, protectedProcedure } from "../trpc";
 import { z } from "zod";
 
-import { desc, eq, schema } from "@acme/db";
-
-import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
-
-export const postRouter = createTRPCRouter({
+export const postRouter = router({
   all: publicProcedure.query(({ ctx }) => {
-    // return ctx.db.select().from(schema.post).orderBy(desc(schema.post.id));
-    return ctx.db.query.post.findMany({ orderBy: desc(schema.post.id) });
+    return ctx.prisma.post.findMany();
   }),
-
-  byId: publicProcedure
-    .input(z.object({ id: z.number() }))
-    .query(({ ctx, input }) => {
-      // return ctx.db
-      //   .select()
-      //   .from(schema.post)
-      //   .where(eq(schema.post.id, input.id));
-
-      return ctx.db.query.post.findFirst({
-        where: eq(schema.post.id, input.id),
-      });
-    }),
-
+  byId: publicProcedure.input(z.string()).query(({ ctx, input }) => {
+    return ctx.prisma.post.findFirst({ where: { id: input } });
+  }),
   create: protectedProcedure
-    .input(
-      z.object({
-        title: z.string().min(1),
-        content: z.string().min(1),
-      }),
-    )
+    .input(z.object({ title: z.string(), content: z.string() }))
     .mutation(({ ctx, input }) => {
-      return ctx.db.insert(schema.post).values(input);
+      return ctx.prisma.post.create({ data: input });
     }),
-
-  delete: protectedProcedure.input(z.number()).mutation(({ ctx, input }) => {
-    return ctx.db.delete(schema.post).where(eq(schema.post.id, input));
-  }),
 });
